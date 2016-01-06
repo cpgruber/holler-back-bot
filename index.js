a4a22c85e41f465c317522a91c44ba8437b0bf9a
Chase Gruber <chasegruber@gmail.com>
2016-01-06 17:49:18 -0500

    tweet a hashtag as a handle, get random tweet in a reply

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var Twit = require('twit');
-// var TwitterBot = require('node-twitterbot').TwitterBot;
 var env = require("./env.js")
 
 var Bot = new Twit({
@@ -13,34 +12,56 @@ var Bot = new Twit({
   // access_token_secret:  process.env.SWTITLEBOT_ACCESS_TOKEN_SECRET
 });
 
-function post (content) {
-  Bot.post('statuses/update', { status: content }, function(err, data, response) {
+function post (content,reply_id) {
+  Bot.post('statuses/update', { status: content, in_reply_to_status_id: reply_id }, function(err, data, response) {
+    console.log(err)
+    console.log("********")
+    console.log(data)
+    console.log("tweeted!")
+  })
+}
+
+function search (hashtag) {
+  var sn = hashtag.replace("#","")
+  return new Promise(function(resolve, reject){
+    Bot.get('statuses/user_timeline', { screen_name: sn, count: 100 }, function(err, data, response) {
+      if (!err){
+        var tweets = data.filter(function(d){
+          return d.text.substring(0,2) !== "RT"
+        }).map(function(d){
+          return d.text.replace("@","").replace("#","");
+        })
+        var random = Math.floor(Math.random() * tweets.length);
+        var content = tweets[random];
+        content.replace("@","")
+      }else{
+        var content = "I don't know who "+sn+" is, and I'm too lazy to find them."
+      }
+      resolve(content);
+    })
   })
 }
 
 var stream = Bot.stream('statuses/filter', { track: '@slouchlife' });
 stream.on('tweet', function (tweet) {
-  console.log(tweet)
   var asker = tweet.user.screen_name;
+  var id = tweet.id_str;
   var text = tweet.text;
-  console.log(asker + " tweeted: " + text);
-  var reply = "@"+asker+" hows it?";
-  post(reply);
-});
-// Bot.post('statuses/update', { status: 'hello world (again... so lazy)!' }, function(err, data, response) {
-//   console.log(data);
-// });
+  var hashtag = tweet.text.split(" ").filter(function(d){return d[0]=="#"})[0];
+  search(hashtag).then(function(tweet){
+    var modTweet = ("@"+asker+": "+tweet).substring(0,139);
+    console.log(modTweet)
+    post(modTweet, id)
+  })
+  // post("@"+asker+": I heard that.",id)
 
-// var listenerFunction = function(tweet) {
-//   console.log(tweet)
-//   if (something)
-//     return true;
-//   return false;
-// }
-//
-// Bot.listen("listening", listenerFunction, function(twitter, action, tweet) {
-//   console.log(twitter)
-//   Bot.now(Bot.tweet("I heard that"));
-// });
-//
-// Bot.tweet("@slouchlife ya boi")
+  // var text = tweet.text;
+  // var hashtag = tweet.text.split(" ").filter(function(d){return d[0]=="#"})[0];
+  // console.log(hashtag)
+  // console.log(asker + " tweeted: " + text);
+  // search(hashtag).then(function(content){
+  //   var reply = "@"+asker+": "+content;
+  //   console.log(reply)
+  //   post(reply);
+  // });
+});
