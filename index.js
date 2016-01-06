dfb9f03c8c24dcde12301e4dc69e1dfbc879861a
Chase Gruber <chasegruber@gmail.com>
2016-01-06 14:48:17 -0500

    reply to @ tweets

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var Twit = require('twit');
-var TwitterBot = require('node-twitterbot').TwitterBot;
+// var TwitterBot = require('node-twitterbot').TwitterBot;
 var env = require("./env.js")
 
-var Bot = new TwitterBot({
+var Bot = new Twit({
   consumer_key: env.consumer_key,
   consumer_secret: env.consumer_secret,
   access_token: env.access_token,
@@ -13,4 +13,34 @@ var Bot = new TwitterBot({
   // access_token_secret:  process.env.SWTITLEBOT_ACCESS_TOKEN_SECRET
 });
 
-Bot.tweet("HELLO I'm lazy")
+function post (content) {
+  Bot.post('statuses/update', { status: content }, function(err, data, response) {
+  })
+}
+
+var stream = Bot.stream('statuses/filter', { track: '@slouchlife' });
+stream.on('tweet', function (tweet) {
+  console.log(tweet)
+  var asker = tweet.user.screen_name;
+  var text = tweet.text;
+  console.log(asker + " tweeted: " + text);
+  var reply = "@"+asker+" hows it?";
+  post(reply);
+});
+// Bot.post('statuses/update', { status: 'hello world (again... so lazy)!' }, function(err, data, response) {
+//   console.log(data);
+// });
+
+// var listenerFunction = function(tweet) {
+//   console.log(tweet)
+//   if (something)
+//     return true;
+//   return false;
+// }
+//
+// Bot.listen("listening", listenerFunction, function(twitter, action, tweet) {
+//   console.log(twitter)
+//   Bot.now(Bot.tweet("I heard that"));
+// });
+//
+// Bot.tweet("@slouchlife ya boi")
